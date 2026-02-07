6eb07cdad550f2614dcca23ef6175fcf7b614e3d
shenonk <sakurukulasuriya@students.nsbm.ac.lk>
2026-02-07 08:48:06 +0530

    Update login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./layout/MainLayout";
 
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import PriceCheck from "./pages/PriceCheck";
 import Results from "./pages/Results";
@@ -12,6 +13,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* LOGIN PAGE - Full screen, no layout */}
+        <Route path="/login" element={<Login />} />
 
         {/* MAIN LAYOUT WRAP */}
         <Route path="/" element={<MainLayout />}>
